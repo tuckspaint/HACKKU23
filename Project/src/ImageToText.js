425992db558d9e6b57477d657abb6103ce2efddd
EuslidEuclid97 <zackandrews29@yahoo.com>
2023-04-15 13:52:56 -0500

    Made minor cosmetic changes

--- Project/src/ImageToText.js
+++ Project/src/ImageToText.js
@@ -1,10 +1,33 @@
 import React, { useCallback } from 'react';
-import { useNavigate } from 'react-router-dom';
 import { useDropzone } from 'react-dropzone';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFileAlt, faSpinner, faTrash } from '@fortawesome/free-solid-svg-icons'
+import './styles/ImageTotext.css'
 
 export default function ImageToText() {
     const onDrop = useCallback(acceptedFiles => {
-        console.log(acceptedFiles);
+        console.log(acceptedFiles)
+        /*return(
+            <>
+            <li
+                className="file-item"
+                key={file.name}>
+                <FontAwesomeIcon icon={faFileAlt} />
+                <p>{file.name}</p>
+                <div className="actions">
+                    <div className="loading"></div>
+                    {file.isUploading && <FontAwesomeIcon
+                        icon={faSpinner} className="fa-spin"
+                        onClick={() => deleteFile(file.name)} />
+                    }
+                    {!file.isUploading &&
+                        <FontAwesomeIcon icon={faTrash}
+                            onClick={() => deleteFile(file.name)} />
+                    }
+                </div>
+            </li>
+        </>
+        )*/
     }, []);
 
     const {
@@ -16,6 +39,7 @@ export default function ImageToText() {
 
     return (
     <div {...getRootProps()}>
+        <p className='title'>Upload file</p>
         <input {...getInputProps()} />
         <button className='dropButton'>Drop your images here!</button>
     </div>
--- Project/src/Landing.js
+++ Project/src/Landing.js
@@ -1,35 +1,52 @@
 import React from 'react'
-import { useState } from 'react';
+import { useState, useEffect, CSSProperties } from 'react';
+import ClipLoader from "react-spinners/ClipLoader"
 import './styles/Landing.css'
+import { blue } from '@mui/material/colors';
 
 export default function Landing() {
 
-    const search = (e) => {
-        e.preventDefault();
-        console.log()
+    const [query, setQuery] = React.useState("");
+    const [resp, setResp] = React.useState("");
+    const [loading, setLoading] = React.useState(false);
+
+    const search = async (e) => {
+        e.preventDefault()
+        /*setLoading(true)
+        setTimeout(() => {
+            setLoading(false)
+        }, 3000)*/
         fetch('http://localhost:3000/chat?q=' + query)
             .then(response => response.text())
-            .then(data => console.log(data));
+            .then(response => setResp(response))
     };
 
-    const [query, setQuery] = React.useState("Differential equations");
     return (
     <div>
         <h1 className='headers'>Explain</h1>
-        <form onSubmit={search}>
-            <input
-                autoFocus
-                value={query}
-                onChange={e => setQuery(e.target.value)}
-            />
-            <button>Search</button>
-        </form>
+        <div className='searchBar'>
+            <form>
+                <input
+                    autoFocus
+                    value={query}
+                    onChange={e => setQuery(e.target.value)}
+                />
+            </form>
+        </div>
         <h1 className='headers'>like I'm...</h1>
         <div className='buttonDiv'>
-            <button onClick = {query && search} className='belowButtons'>5 years old</button>
-            <button onClick = {query && search} className='belowButtons'>In high school</button>
-            <button onClick = {query && search} className='belowButtons'>In college</button>
-            <button onClick = {query && search} className='belowButtons'>An expert</button>
+            <button onClick = {search} className='belowButtons'>5 years old</button>
+            <button onClick = {search} className='belowButtons'>In high school</button>
+            <button onClick = {search} className='belowButtons'>In college</button>
+            <button onClick = {search} className='belowButtons'>An expert</button>
+        </div>
+        <div className='output'>
+        {loading ? <ClipLoader
+               color={blue}
+               loading={loading}
+               size={100}
+           /> : resp  
+            }
         </div>
     </div>
     )
