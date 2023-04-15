88146bed17844dd2a375327932bb4e4dd2a5da18
tuckspaint <tuckercumberbatch5ever@gmail.com>
2023-04-15 04:13:51 -0500

    image to text

--- Project/server/server.js
+++ Project/server/server.js
@@ -2,6 +2,51 @@ const { Configuration, OpenAIApi } = require("openai");
 const express = require('express')
 const bparse = require('body-parser');
 const cors = require('cors');
+const vision = require('@google-cloud/vision');
+const {Storage} = require('@google-cloud/storage');
+
+
+
+// async function authenticateImplicitWithAdc() {
+
+//   // This snippet demonstrates how to list buckets.
+//   // NOTE: Replace the client created below with the client required for your application.
+//   // Note that the credentials are not specified when constructing the client.
+//   // The client library finds your credentials using ADC.
+//   const storage = new Storage({
+//     auth: (process.env.GCP_API_KEY),
+//   });
+//   const [buckets] = await storage.getBuckets();
+//   console.log('Buckets:');
+
+//   for (const bucket of buckets) {
+//     console.log(`- ${bucket.name}`);
+//   }
+
+//   console.log('Listed all storage buckets.');
+// }
+
+// authenticateImplicitWithAdc();
+// Creates a client
+const client = new vision.ImageAnnotatorClient({
+  keyFilename: "key.json"
+});
+
+
+async function ImageToText() {
+/**
+ * TODO(developer): Uncomment the following line before running the sample.
+ */
+// const fileName = 'Local image file, e.g. /path/to/image.png';
+const fileName = 'text.jpg';
+// Performs text detection on the local file
+const [result] = await client.textDetection(fileName);
+const detections = result.textAnnotations;
+console.log('Text:');
+detections.forEach(text => console.log(text));
+}
+
+ImageToText()
 
 // Express application
 const app = express()
