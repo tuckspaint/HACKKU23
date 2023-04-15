eef429d427d499cd2105337c21caba461367bcee
EuslidEuclid97 <zackandrews29@yahoo.com>
2023-04-15 11:26:18 -0500

    Added initial simple GPT output

--- Project/src/ImageToText.js
+++ Project/src/ImageToText.js
@@ -1,7 +1,23 @@
-import React from 'react'
+import React, { useCallback } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDropzone } from 'react-dropzone';
 
 export default function ImageToText() {
-  return (
-    <div>ImageToText</div>
-  )
+    const onDrop = useCallback(acceptedFiles => {
+        console.log(acceptedFiles);
+    }, []);
+
+    const {
+    getRootProps,
+    getInputProps
+    } = useDropzone({
+    onDrop
+    });
+
+    return (
+    <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        <button className='dropButton'>Drop your images here!</button>
+    </div>
+    )
 }
