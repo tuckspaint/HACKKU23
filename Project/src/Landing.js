557dc1ac8697f71b9cdec676b9a7c761fef13c9f
EuslidEuclid97 <zackandrews29@yahoo.com>
2023-04-15 14:14:42 -0500

    Made some changes to Landing

--- Project/src/Landing.js
+++ Project/src/Landing.js
@@ -6,8 +6,8 @@ import { blue } from '@mui/material/colors';
 
 export default function Landing() {
 
-    const search = (e) => {
-        e.preventDefault();
+    const search = (level) => {
+        setLevel(level)
         console.log(level)
         fetch('http://localhost:3000/chat?q=' + query + "&l=" + level)
             .then(response => response.text())
@@ -16,6 +16,7 @@ export default function Landing() {
 
     const [query, setQuery] = React.useState("Differential equations");
     const [level, setLevel] = React.useState(0);
+    const [resp, setResp] = React.useState("")
     return (
     <div>
         <h1 className='headers'>Explain</h1>
@@ -30,10 +31,13 @@ export default function Landing() {
         </div>
         <h1 className='headers'>like I'm...</h1>
         <div className='buttonDiv'>
-            <button className='belowButtons' onClick={() => setLevel(0)}>5 years old</button>
-            <button className='belowButtons' onClick={() => setLevel(1)}>In high school</button>
-            <button className='belowButtons' onClick={() => setLevel(2)}>In college</button>
-            <button className='belowButtons' onClick={() => setLevel(3)}>An expert</button>
+            <button className='belowButtons' onClick={() => {search(0)}}>5 years old</button>
+            <button className='belowButtons' onClick={() => {search(1)}}>In high school</button>
+            <button className='belowButtons' onClick={() => {search(2)}}>In college</button>
+            <button className='belowButtons' onClick={() => {search(3)}}>An expert</button>
+        </div>
+        <div className='output'>
+            {resp}
         </div>
     </div>
     )
